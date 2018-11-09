4b7985181462895fc9b7efab4081c687baa81d21
Stuart Hicks <stuart.hicks@gmail.com>
2018-11-09 13:27:30 +0100

    Added react-native-maps for iOs
    All the stuff that's been added including pods.
    A build workspace.
    The minimal code required to include the map in the MapScreen.

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import MapView from 'react-native-maps';
+
+const region = {
+  latitude: 37.78825,
+  longitude: -122.4324,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
+};
 
 class MapScreen extends Component {
+
   render() {
     return (
-      <View>
-        <Text>MapScreen</Text>
+      <View style={styles.container}>
+        <MapView
+          region={region}
+          style={styles.map}
+        />
       </View>
     );
   }
 }
+
+const styles = {
+ container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  }
+};
 
 export { MapScreen };
